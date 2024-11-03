f4bb837f218240a23d1c778a745aa00ad751a913
Yashas <yashasjob@gmail.com>
2024-11-03 23:05:20 +0530

    c1

---
+++ src/redux/reducer/journeyReducer.js
@@ -0,0 +1,15 @@
+import { JOURNEY_DETAILS } from "../actions/actionTypes";
+import storeJourneydata from "../actions/journeyDetailsActions";
+
+let initialState = null;
+
+const journeyReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case JOURNEY_DETAILS:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default journeyReducer;
